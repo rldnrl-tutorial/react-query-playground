c51fed879c34245b9fe47e7a832698a749937fe8
rldnrl <devpark1002@gmail.com>
2022-02-12 12:02:35 +0900

    feat: implement fetch comments logic

---
+++ apis/fetchComments.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { BASE_URL } from "./config";
+
+export type Comment = {
+  postId: number
+  id: number
+  name: string
+  email: string
+  body: string
+}
+
+const fetchComments = async (postId: number) => {
+  const response = await axios.get<Comment[]>(`${BASE_URL}/comments?postId=${postId}`)
+
+  return response.data
+}
+
+export default fetchComments
